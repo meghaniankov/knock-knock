75ab3e591598325b268c7b458978a091eee1c9c5
Meghan <meghan.e.mccue@gmail.com>
2020-05-03 12:08:29 +0100

    Remove StatusContainer from app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
-// import StatusContainer from './components/StatusContainer';
 import UserContainer from './components/UserContainer';
 
 
 function App() {
   return (
     <UserContainer />
-    // <StatusContainer />
   );
 }
 
