6b5e6154de184cfefe72bf1c883d7707e45e7f62
meghaniankov <meghan.e.mccue@gmail.com>
2021-02-07 16:46:09 +0000

    add serverside authentication for sign up

--- client/src/components/users/user-auth.jsx
+++ client/src/components/users/user-auth.jsx
@@ -1,34 +1,70 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import axios from 'axios'
 
- 
 const UserAuthentication = () => {
-   const [username, setUsername] = React.useState('')
+  const [username, setUsername] = React.useState('')
+  const [users, setUsers] = React.useState([])
 
-   const onChangeUsername = (e) => {
-      const { value } = e.target
-      setUsername(value)
-   }
+  const getData = async() => {
+   const response = await fetch('http://localhost:5000/users/')
+   const data = await response.json()
+   setUsers(data)
+  }
 
-   const onSubmit = () => {
-     axios.post('http://localhost:5000/users/', {username: username})
-   }
+  useEffect(() => {
+   getData()
+  }, [])
 
-   return (
-    <form class="form-inline" onSubmit={() => onSubmit()}>
-      <label class="sr-only" for="inlineFormInputGroupUsername2">Username</label>
+  const onChangeUsername = (e) => {
+    const { value } = e.target
+    setUsername(value)
+  }
+
+  const onClickSignUp = () => {
+    if (usernameIsValid() && usernameIsUnique()) {
+      axios.post('http://localhost:5000/users/', {username: username})
+    } else {
+      console.log('User already exists')
+    }
+  }
+
+  const onClickLogIn = () => {
+    if (usernameIsUnique() === true) {
+      console.log('log me in')
+    }
+  }
+
+  const usernameIsValid = () => {
+    if (username === '') return false
+    return true
+  }
+
+  const usernameIsUnique = () => {
+    let usernameExists = false
+
+    users.map((user) => {
+      if (username === user.username) {
+        usernameExists = true
+      }
+    })
+
+    return usernameExists
+  }
+
+  return (
+    <div>
       <input 
         type="text" 
         class="form-control" 
         value={username}
         onChange={onChangeUsername}
+        style={{maxWidth: '250px'}}
         id="inlineFormInputGroupUsername2" 
         placeholder="Username" />
-      <button type="submit" class="btn btn-primary mb-2">Log In</button>
-      <button type="submit" class="btn btn-primary mb-2">Sign Up</button>
-    </form>
-
-   )
+      <button type="submit" class="btn btn-primary mb-2" onClick={() => onClickLogIn()}>Log In</button>
+      <button type="submit" class="btn btn-primary mb-2" onClick={() => onClickSignUp()}>Sign Up</button>
+    </div>
+  )
 }
 
 export default UserAuthentication
